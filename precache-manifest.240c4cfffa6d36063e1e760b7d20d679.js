9343bc6ca43e5463a9ec9854cef3a25081bb0fa8
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2020-05-05 14:20:48 -0400

    Updates

---
+++ precache-manifest.240c4cfffa6d36063e1e760b7d20d679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56d03fc3abead171a099cf384d68649",
+    "url": "/reactjs-directory-app/index.html"
+  },
+  {
+    "revision": "f1d1d2e22b49b239fb91",
+    "url": "/reactjs-directory-app/static/css/main.e1d5110d.chunk.css"
+  },
+  {
+    "revision": "e070a2a3483ca03de17b",
+    "url": "/reactjs-directory-app/static/js/2.fdf9b88d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-directory-app/static/js/2.fdf9b88d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d1d2e22b49b239fb91",
+    "url": "/reactjs-directory-app/static/js/main.664ecb1c.chunk.js"
+  },
+  {
+    "revision": "b7495e9d13cbfa851817",
+    "url": "/reactjs-directory-app/static/js/runtime-main.dfe9ab2c.js"
+  }
+]);
